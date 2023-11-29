ddae3812c7eaf33e9742fc2690591a3f926297d9
empowerFiodar <fiodar@empower.me>
2023-11-29 16:15:20 +0000

    Add a missing class

--- Virtual-assistant/VirtualAssistant/VirtualAssistant/StockForecaster.cs
+++ Virtual-assistant/VirtualAssistant/VirtualAssistant/StockForecaster.cs
@@ -7,15 +7,15 @@ internal static class StockForecaster
     public static void ForecastStockPrices()
     {
         Console.WriteLine("How many working days do you want to predict stock prices for?");
-        var horizon = int.Parse(Console.ReadLine());
+        int horizon = int.Parse(Console.ReadLine() ?? string.Empty);
 
-        var input = new ModelInput();
+        ModelInput input = new();
 
-        var predictions = Predict(input, horizon);
+        ModelOutput predictions = Predict(input, horizon);
 
         Console.WriteLine("Forecasted stock prices:");
 
-        foreach (var stockPrice in predictions.AdjClose)
+        foreach (float stockPrice in predictions.AdjClose)
         {
             Console.WriteLine(stockPrice);
         }
